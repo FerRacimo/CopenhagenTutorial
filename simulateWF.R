87c8f6905c8f425276376de715fe72e44bdece6f
Fernando Racimo <fernandoracimo@gmail.com>
2018-07-28 20:02:42 +0200

    Add files via upload

--- simulateWF.R
+++ simulateWF.R
@@ -1,8 +1,84 @@
 # Wright-Fisher simulator written by Graham Coop
 
-par(mar=c(2,2,2,2))
+#par(mar=c(2,2,2,2))
 
-track_lineages<-function(N.vec, n.iter, num.tracked, col.allele=c("red","blue","orange","green","yellow","brown")){
+
+
+WF_manyalleles <- function(N,num.gens){
+offset<-0.1
+plot(c(1,num.gens),c(1,N)+c(-offset,offset),type="n",axes=FALSE,xlab="",ylab="")
+mtext(side=1,line=1,"Generations")
+
+
+ my.cols<-sample(rainbow(2*N))
+ 
+	points(rep(1,N),1:N+offset, pch=19,cex=1.2,col=my.cols[(1:N)*2])
+ points(rep(1,N),1:N-offset, pch=19,cex=1.2,col=my.cols[(1:N)*2-1])
+
+for(i in 1:num.gens){
+ new.cols<-rep("black",2*N)
+
+ for(ind in 1:N){
+		par<-sample(1:N,2,replace=FALSE)
+
+		which.allele<-sample(c(-1,1),1)
+		lines(c(i,i+1), c(par[1]+which.allele*offset,ind-offset),col="grey",lwd=0.5)
+		new.cols[2*ind-1]<- my.cols[2*par[1] +ifelse(which.allele==1,0,-1)]
+
+		which.allele<-sample(c(-1,1),1)
+		lines(c(i,i+1), c(par[2]+which.allele*offset,ind+offset),col="grey",lwd=0.5)
+		new.cols[2*ind]<- my.cols[2*par[2] +ifelse(which.allele==1,0,-1)]
+
+	}
+	
+ 
+ points(rep(i,N),1:N+offset, pch=19,cex=1.2,col=my.cols[(1:N)*2])
+ points(rep(i,N),1:N-offset, pch=19,cex=1.2,col=my.cols[(1:N)*2-1])
+ my.cols<-new.cols
+
+}
+}
+
+
+WF_twoalleles <- function(N,num.gens){
+offset<-0.1
+plot(c(1,num.gens+1),c(1,N)+c(-offset,offset),type="n",axes=FALSE,xlab="",ylab="")
+mtext(side=1,line=1,"Generations")
+ my.cols<-  rep(c("blue","red"),N)  # sample(rep(c("blue","red"),N), size=2*N)
+ 
+	points(rep(1,N),1:N+offset, pch=19,cex=1,col=my.cols[(1:N)*2])
+ points(rep(1,N),1:N-offset, pch=19,cex=1,col=my.cols[(1:N)*2-1])
+my.freq<-numeric()
+for(i in 1:num.gens){
+ new.cols<-rep("black",2*N)
+
+ for(ind in 1:N){
+		par<-sample(1:N,2,replace=FALSE)
+
+		which.allele<-sample(c(-1,1),1)
+		lines(c(i,i+1), c(par[1]+which.allele*offset,ind-offset),col="grey",lwd=0.5)
+		new.cols[2*ind-1]<- my.cols[2*par[1] +ifelse(which.allele==1,0,-1)]
+
+		which.allele<-sample(c(-1,1),1)
+		lines(c(i,i+1), c(par[2]+which.allele*offset,ind+offset),col="grey",lwd=0.5)
+		new.cols[2*ind]<- my.cols[2*par[2] +ifelse(which.allele==1,0,-1)]
+
+	}
+	
+my.freq<- c(my.freq,sum(my.cols=="blue"))
+ points(rep(i,N),1:N+offset, pch=19,cex=1,col=my.cols[(1:N)*2])
+ points(rep(i,N),1:N-offset, pch=19,cex=1,col=my.cols[(1:N)*2-1])
+ my.cols<-new.cols
+
+}
+
+return(my.freq)
+}
+
+
+
+
+track_lineages<-function(N.vec, n.iter, num.tracked, col.allele=c("red","blue","orange","green","yellow","brown","black","pink")){
 num.gens<-length(N.vec)
 offset<-0.2
 for(iter in 1:n.iter){
@@ -55,5 +131,4 @@ for(iter in 1:n.iter){
  	}
 	
 }
-
 
